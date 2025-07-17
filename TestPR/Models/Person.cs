5c3050dce93e734ccb6762e88ab15cdf4c6a22ad
Minuk Lee <alsdnr0716@naver.com>
2025-07-17 15:02:58 +0900

    사람 객체 추가

---
+++ TestPR/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPR.Models
+{
+    public class Person
+    {
+        #region Field
+
+        #endregion
+
+        #region Property
+        public string Name { get; set; }
+        #endregion
+
+        #region Constructor
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+        #endregion
+    }
+}
