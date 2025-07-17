e9a158b567edec89f4816ca5f14601d5f64029c5
최현지 <djaak09255@naver.com>
2025-07-17 15:09:57 +0900

    라벨 크기 변경

--- TestPR/Form1.Designer.cs
+++ TestPR/Form1.Designer.cs
@@ -36,9 +36,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Font = new System.Drawing.Font("굴림", 15F);
+            this.label1.Location = new System.Drawing.Point(12, 88);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.Size = new System.Drawing.Size(57, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
